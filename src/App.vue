57ec1a94c5ec73d9d1c92c382157c89aa95b15f9
saltfish233 <1603321645@qq.com>
2022-07-14 14:03:07 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -17,7 +17,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Homi</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
