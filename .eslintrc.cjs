ae3d1d994e86d4d2355e3f99abeb2cea55520128
saltfish233 <1603321645@qq.com>
2022-07-14 13:53:49 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  rules: {
+    indent: 0,
+    'space-before-function-paren': 0
+  }
+}
