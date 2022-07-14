6605006061b7e616149acaf0e83e4da73b76168a
saltfish233 <1603321645@qq.com>
2022-07-14 14:07:11 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Homi</RouterLink>
+        <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
