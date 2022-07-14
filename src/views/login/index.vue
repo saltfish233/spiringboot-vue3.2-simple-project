bf805ed8c14a7fb773e807946d252984a0488f5a
saltfish233 <1603321645@qq.com>
2022-07-14 18:23:01 +0800

    feat: test

---
+++ src/views/login/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+const form = {
+  username: '',
+  password: ''
+}
+</script>
+
+<template>
+  <el-form
+    :model="form"
+    ref="form"
+    :rules="rules"
+    label-width="80px"
+    :inline="false"
+    size="normal"
+  >
+    <el-form-item label="">
+      <el-input v-model="form.username"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<style lang="scss"></style>
